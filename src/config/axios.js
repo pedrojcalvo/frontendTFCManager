9ff78c18e04493719edae3b4c15c5e3a5fdaed46
Pedro Calvo <pedrojcalvo@gmail.com>
2022-06-06 22:05:58 +0200

    configurando la base de datos

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,5 +4,5 @@ import axios from 'axios';
 console.log(process.env.REACT_APP_BACK_URL);
 
 export const backAxiosClient = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL,
+    baseURL: process.env.REACT_APP_BACK_URL || "http://localhost:3000/",
 });
